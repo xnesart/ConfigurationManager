75c8dadfaf8495588d3d30d77c1bc279a9b88b0b
xnesart <xnesart@mail.ru>
2024-06-13 16:57:18 +0300

    Added check on exist for new configuration in database

--- ServiceConfigManager.DataLayer/Repositories/ConfigurationRepository.cs
+++ ServiceConfigManager.DataLayer/Repositories/ConfigurationRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 using ServiceConfigManager.Core;
 using ServiceConfigManager.Core.DTOs;
@@ -8,6 +9,7 @@ public class ConfigurationRepository : IConfigurationRepository
 {
     protected readonly DataBaseContext _ctx;
     private readonly ILogger _logger = Log.ForContext<ConfigurationRepository>();
+
     public ConfigurationRepository(DataBaseContext ctx)
     {
         _ctx = ctx;
@@ -16,12 +18,22 @@ public class ConfigurationRepository : IConfigurationRepository
     public async Task<Guid> AddConfigurationForService(ServiceConfigurationDto newConfiguration)
     {
         _logger.Information($"Сервисы: добавление конфигурации: добавляем новую конфигурацию в контекст");
-
+        
+        if (await CheckForExistingConfiguration(newConfiguration)) throw new InvalidOperationException("Такая конфигурация уже существует, используйте PUT endpoint");
+        
         await _ctx.AddAsync(newConfiguration);
         await _ctx.SaveChangesAsync();
-        
+
         _logger.Information($"Возвращаем айди конфигурации {newConfiguration.Id}");
 
         return newConfiguration.Id;
+    }
+
+    private async Task<bool> CheckForExistingConfiguration(ServiceConfigurationDto newConfiguration)
+    {
+        _logger.Information($"Проверяем, существует ли в базе такая конфигурация {newConfiguration.ServiceType}");
+        var exists = await _ctx.ServiceConfiguration.AnyAsync(t => t.ServiceType == newConfiguration.ServiceType);
+        
+        return exists;
     }
 }
